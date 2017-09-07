a2c1893c2e2c3e3f7f761c406c39d5e790644c01
LAPTOP-CAQF635U\rakesh <rakesh.ihs55@gmail.com>
2017-09-07 12:04:56 -0500

    Added a call back error functions

--- app.js
+++ app.js
@@ -20,8 +20,15 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
-    // console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longittude: ${body.results[0].geometry.location.lng}`);    
+    if (error) {
+        console.log('Unable to connect google api.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longittude: ${body.results[0].geometry.location.lng}`);    
+    } else if (body.status === 'INVALID_REQUEST') {
+        console.log('Invalid Request.');
+    }
 });
