00996a57c05db5830747c29a8aa720de6f791875
LAPTOP-CAQF635U\rakesh <rakesh.ihs55@gmail.com>
2017-09-07 14:37:55 -0500

    Simplified app file into access a global geocode function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,21 +15,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
- var encodedAddress = encodeURIComponent(argv.a);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) =>{
-    if (error) {
-        console.log('Unable to connect google api.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find the address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longittude: ${body.results[0].geometry.location.lng}`);    
-    } else if (body.status === 'INVALID_REQUEST') {
-        console.log('Invalid Request.');
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
